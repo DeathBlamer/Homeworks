0ddb93b8c63e08b28a498468ad2a2871f0219a50
DeathBlamer <deathblamer@gmail.com>
2023-06-21 22:42:09 +0500

    add Homework_1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,64 @@
+﻿//Task 2  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// System.Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     System.Console.WriteLine($"Число {num1} больше");
+// }
+// else
+// {
+//     System.Console.WriteLine($"Число {num2} больше");
+// }
+
+
+
+//Task 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// System.Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите третье число -> ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num1 > max) max = num1;
+// if (num2 > max) max = num2;
+// if (num3 > max) max = num3;
+
+// Console.WriteLine($"Максимальное чиcло из трёх -> {max}");
+
+
+
+//Task 6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// System.Console.Write("Введите число -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if ((num % 2) == 0)
+// {
+//     System.Console.WriteLine("Число четное");
+// }
+// else
+// {
+//     System.Console.WriteLine("Число нечетное");
+// }
+
+
+
+//Task 8  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// System.Console.Write("Введите число -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int count = 1;
+
+// while (count <= n)
+// {
+//     if(count % 2 == 0)
+//     {
+//         System.Console.Write($"{count} ");
+//     }
+//     count++;
+// }
