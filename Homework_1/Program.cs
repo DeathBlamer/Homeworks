37a520893dfb371d0421172eef11889711b67e24
DeathBlamer <deathblamer@gmail.com>
2023-06-22 17:53:02 +0500

    Add Homework_2 Task_10

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -16,19 +16,19 @@
 
 
 //Task 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// System.Console.Write("Введите первое число -> ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// System.Console.Write("Введите второе число -> ");
-// int num2 = Convert.ToInt32(Console.ReadLine());
-// System.Console.Write("Введите третье число -> ");
-// int num3 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите первое число -> ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число -> ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число -> ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-// int max = num1;
+int max = num1;
 
-// if (num2 > max) max = num2;
-// if (num3 > max) max = num3;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 
-// Console.WriteLine($"Максимальное чиcло из трёх -> {max}");
+Console.WriteLine($"Максимальное чиcло из трёх -> {max}");
 
 
 
---
+++ Homework_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// // Task 10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int SecondNum (int threedigit)
+// {
+//     int num1 = (threedigit / 10 % 10);
+//     return num1;
+// }
+
+// System.Console.Write("Введите трёхзначное число -> ");
+// int threedigit = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(SecondNum(threedigit));
