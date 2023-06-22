2a48151b38259fcd6b88a6bdbb78137368baf156
DeathBlamer <deathblamer@gmail.com>
2023-06-22 20:55:16 +0500

    Add Homewrok 2

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,4 +1,5 @@
-﻿//Task 2  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Task 2  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
 // System.Console.Write("Введите первое число -> ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // System.Console.Write("Введите второе число -> ");
@@ -15,24 +16,24 @@
 
 
 
-//Task 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-System.Console.Write("Введите первое число -> ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите второе число -> ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите третье число -> ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Task 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// System.Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите третье число -> ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max = num1;
+// int max = num1;
 
-if (num2 > max) max = num2;
-if (num3 > max) max = num3;
+// if (num2 > max) max = num2;
+// if (num3 > max) max = num3;
 
-Console.WriteLine($"Максимальное чиcло из трёх -> {max}");
+// Console.WriteLine($"Максимальное чиcло из трёх -> {max}");
 
 
 
-//Task 6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Task 6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 // System.Console.Write("Введите число -> ");
 // int num = Convert.ToInt32(Console.ReadLine());
 
@@ -47,7 +48,7 @@ Console.WriteLine($"Максимальное чиcло из трёх -> {max}");
 
 
 
-//Task 8  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Task 8  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 // System.Console.Write("Введите число -> ");
 // int n = Convert.ToInt32(Console.ReadLine());
 
--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,4 +1,4 @@
-﻿// // Task 10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// Task 10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 // int SecondNum (int threedigit)
 // {
@@ -9,4 +9,47 @@
 // System.Console.Write("Введите трёхзначное число -> ");
 // int threedigit = Convert.ToInt32(Console.ReadLine());
 
-// System.Console.WriteLine(SecondNum(threedigit));
+// System.Console.WriteLine($"Второе число -> {SecondNum(threedigit)}");
+
+
+
+// Task 13 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// int ThirdNum (int number)
+// {
+//     if (number > 99)
+//     {
+//         while (number > 999)
+//         {
+//             number = number / 10;
+//         }
+//     }
+//     int num1 = number % 10;
+//     return num1;
+// }
+
+// System.Console.Write("Введите число -> ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number > 99)
+// {
+//     System.Console.WriteLine(ThirdNum (number));
+// }
+// else
+// {
+//     System.Console.WriteLine("Третьей цифры нет");
+// }
+
+
+// Task 15 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// bool CheckWeekend (int day)
+// {
+//     if (day == 6 || day == 7) return true;
+//     else return false;
+// }
+
+// System.Console.Write("Введите день -> ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(CheckWeekend(day));
