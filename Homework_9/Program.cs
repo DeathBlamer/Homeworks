3076e89c190529fdfe8cff5beefcdc5b203d26d2
DeathBlamer <deathblamer@gmail.com>
2023-10-11 11:50:36 +0500

    Add "Homework 9"

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -1,15 +1,71 @@
-﻿//Задача 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
-
-//N = 5 -> "1, 2, 3, 4, 5"
-//N = 6 -> "1, 2, 3, 4, 5, 6"
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 // void PrintNum (int n)
 // {
 //     if (n >= 1)
 //     {
-//         PrintNum(n-1);
 //         Console.Write(n + " ");
+//         PrintNum(n-1);
 //     }
 // }
 
+// System.Console.WriteLine("Input N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
 
+// PrintNum(numberN);
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int IntSum(int m, int n)
+// {
+//     if (m != n)
+//     {
+//         if (m < n)
+//         {
+//             return IntSum(m, n - 1) + n;
+//         }
+        
+//         {
+//             return IntSum(m -1, n) + m;   
+//         }
+//     }
+//     return m;
+// }
+
+// System.Console.WriteLine("Input M: ");
+// int numberM = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine();
+
+// System.Console.WriteLine(IntSum(numberM, numberN));
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int AckermannFunction(int m, int n)
+//     {
+//         if (m == 0)
+//         {
+//             return n + 1;
+//         }
+//         else if (n == 0)
+//         {
+//             return AckermannFunction(m - 1, 1);
+//         }
+//         else
+//         {
+//             return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+//         }
+//     }
+
+// System.Console.WriteLine(AckermannFunction(2, 2));
